16052892df29cb002272c561214c76afd4186f26
Faiza Hanif <faizahanif-math@users.noreply.github.com>
2026-09-25 17:52:24 +0500

    Add SSL mode support for Neon cloud database

--- dashboard_utils.py
+++ dashboard_utils.py
@@ -2,10 +2,14 @@ import psycopg
 import streamlit as st
 
 def get_connection():
-    return psycopg.connect(
-        host=st.secrets["postgres"]["host"],
-        port=st.secrets["postgres"]["port"],
-        dbname=st.secrets["postgres"]["dbname"],
-        user=st.secrets["postgres"]["user"],
-        password=st.secrets["postgres"]["password"]
-    )
+    params = {
+        "host": st.secrets["postgres"]["host"],
+        "port": st.secrets["postgres"]["port"],
+        "dbname": st.secrets["postgres"]["dbname"],
+        "user": st.secrets["postgres"]["user"],
+        "password": st.secrets["postgres"]["password"],
+    }
+    # Optional SSL mode (needed for Neon cloud database)
+    if "sslmode" in st.secrets["postgres"]:
+        params["sslmode"] = st.secrets["postgres"]["sslmode"]
+    return psycopg.connect(**params)
